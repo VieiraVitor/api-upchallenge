735c46a25b3660a638a44f66826954f056dbd724
VieiraVitor <vieirachp@hotmail.com>
2020-08-01 19:07:00 -0300

    feat: adiciona função para validar transação

--- src/controllers/validate-controller.js
+++ src/controllers/validate-controller.js
@@ -13,3 +13,32 @@ exports.validate = (req, res, next) => {
     );
 
 };
+
+function validateTransaction(dataTransaction) {
+    const customer = dataTransaction.customer
+    let score = 0;
+
+    // validar ip location da transação e state do customer
+    console.log(customer.state)
+    console.log(dataTransaction.ip_location)
+    if (customer.state !== dataTransaction.ip_location) {
+        // TODO implementar logica pra aumentar o score
+        score += 100;
+        console.log(score)
+    }
+
+    if (customer.name !== dataTransaction.card_hold_name) {
+        // TODO implementar logica pra aumentar o score
+        score += 115;
+        console.log(score)
+    }
+
+    if (!validateState(customer.phone, dataTransaction.ip_location)) {
+        score += 25;
+        console.log(score)
+    }
+
+    return score;
+}
+
+
