59b1f986c10524fd5084d61457e92e2f2f3fcff5
VieiraVitor <vieirachp@hotmail.com>
2020-08-01 19:22:25 -0300

    refactor: cria arquivo validate-helper para organizar as funções de validação

---
+++ src/helpers/validate-helper.js
@@ -0,0 +1,45 @@
+const resultTransaction = function validateTransaction(dataTransaction) {
+    const customer = dataTransaction.customer
+    let score = 0;
+
+    // validar ip location da transação e state do customer
+    console.log(customer.state)
+    console.log(dataTransaction.ip_location)
+    if (customer.state !== dataTransaction.ip_location) {
+        // TODO implementar lógica pra aumentar o score
+        score += 100;
+        console.log(score)
+    }
+
+    if (customer.name !== dataTransaction.card_hold_name) {
+        // TODO implementar lógica pra aumentar o score
+        score += 115;
+        console.log(score)
+    }
+
+    if (!validateState(customer.phone, dataTransaction.ip_location)) {
+        // TODO implementar lógica pra aumentar o score
+        score += 25;
+        console.log(score)
+    }
+
+    return score;
+}
+
+// Comparar o DDD do phone do cliente com o UF do state da localização da transação
+function validateState(phoneCustomer, locationTransaction) {
+
+    const ddd = phoneCustomer.slice(0, 2)
+
+    // descobrir state pelo ddd
+    const state = findStateByDDD(ddd);
+
+    //comparar state do ddd com state da transação
+    if (state === locationTransaction) {
+        return true
+    } else {
+        return false
+    }
+}
+
+module.exports = resultTransaction;
